6e709ad914969b474280258a743cf94c426f5f52
vlzuiev <zuiev.v@icloud.com>
2018-12-24 18:43:07 +0000

    Firs changes

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+	$('.skillbar').each(function(){
+		$(this).find('.skillbar-bar').animate({
+			width:$(this).attr('data-percent')
+		},6000);
+	});
+});
+
+$('.Count').each(function () {
+  var $this = $(this);
+  $({ Counter: 0 }).animate({ Counter: $this.text() }, {
+    duration: 6000,
+    easing: 'swing',
+    step: function () {
+      $this.text(Math.ceil(this.Counter));
+    }
+  });
+});
