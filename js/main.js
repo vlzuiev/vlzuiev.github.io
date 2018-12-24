4dab5ff76890a10972ca858f60c3e1ed89a67255
vlzuiev <zuiev.v@icloud.com>
2018-12-24 19:20:44 +0000

    2

--- js/main.js
+++ js/main.js
@@ -2,14 +2,14 @@ $(document).ready(function(){
 	$('.skillbar').each(function(){
 		$(this).find('.skillbar-bar').animate({
 			width:$(this).attr('data-percent')
-		},6000);
+		},3000);
 	});
 });
 
 $('.Count').each(function () {
   var $this = $(this);
   $({ Counter: 0 }).animate({ Counter: $this.text() }, {
-    duration: 6000,
+    duration: 3000,
     easing: 'swing',
     step: function () {
       $this.text(Math.ceil(this.Counter));
