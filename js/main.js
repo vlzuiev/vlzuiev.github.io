10d5784e6f79fc7b6848e19ef610bdb137cb2d48
vlzuiev <zuiev.v@icloud.com>
2018-12-25 03:00:20 +0000

    edit

--- js/main.js
+++ js/main.js
@@ -3,7 +3,24 @@ $(document).ready(function(){
 		$(this).find('.skillbar-bar').animate({
 			width:$(this).attr('data-percent')
 		},3000);
-	});
+    });
+    
+    $(document).scroll(function() { 
+        scroll_pos = $(this).scrollTop();
+        if(scroll_pos > 10) {
+            $("#top-navbar").css({
+                backgroundColor: 'rgba(245, 245, 245, 0.9)',
+	            transition: 'background-color .5s ease-in-out'
+                
+            });
+        }  else{
+            $("#top-navbar").css({
+                backgroundColor: 'transparent',
+	            transition: 'background-color .5s ease-in-out'
+                
+            });
+        }
+    });
 });
 
 $('.Count').each(function () {
