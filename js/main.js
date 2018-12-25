907569e401f1d187671b033d1e33a41e872ac3c6
vlzuiev <zuiev.v@icloud.com>
2018-12-25 21:31:33 +0000

    update v2

--- js/main.js
+++ js/main.js
@@ -18,6 +18,19 @@ $(document).ready(function () {
         }
     });
 
+    $(".navbar-brand").on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+
+                window.location.hash = hash;
+            });
+        }
+    });
+
     $(document).on('click', '.cta', function () {
         $(this).toggleClass('active')
     })
