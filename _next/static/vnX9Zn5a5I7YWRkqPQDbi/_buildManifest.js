7698fbc04bbc069ad8d16605117d3e4348ffc4fc
Vladyslav Zuiev <vladyslav.zuiev@bshg.com>
2022-12-28 17:41:19 +0100

    new stuff

---
+++ _next/static/vnX9Zn5a5I7YWRkqPQDbi/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-9300ee56e33383aa.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
